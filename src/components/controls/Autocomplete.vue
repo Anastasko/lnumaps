d399b1c9690c25bc56f07d1b62ed59d8f432ea29
lorry <volodja.extrim@gmail.com>
2018-02-13 20:48:33 +0200

    autocomplete

--- src/components/controls/Autocomplete.vue
+++ src/components/controls/Autocomplete.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <md-autocomplete :md-options="options">
+  <md-autocomplete :value="findSelected" @md-changed="changed" @md-selected="selected1" :md-options="options">
     <label>{{label}}</label>
 
     <template slot="md-autocomplete-item" slot-scope="{ item, term }">
@@ -10,6 +10,7 @@
     <template slot="md-autocomplete-empty" slot-scope="{ term }">
       No items matching "{{ term }}" were found. <a @click="createNew(term)">Create a new</a> one!
     </template>
+
   </md-autocomplete>
 
 </template>
@@ -19,12 +20,40 @@ export default {
   props: ['value', 'label', 'options'],
   data: function () {
     return {
-      selected: null
+      selected: this.value
+    }
+  },
+  computed: {
+    findSelected () {
+      let result = ''
+      if (this.selected) {
+        this.options.forEach(o => {
+          if (o.id === this.selected.id) {
+            result = o.text
+          }
+        })
+      }
+      return result
+    }
+  },
+  watch: {
+    selected (val) {
+      this.$emit('input', val)
     }
   },
   methods: {
     createNew (text) {
       console.log(text)
+    },
+    selected1 (item) {
+      this.selected = {
+        id: item.id
+      }
+    },
+    changed (text) {
+      if (!text) {
+        this.selected = null
+      }
     }
   }
 }
